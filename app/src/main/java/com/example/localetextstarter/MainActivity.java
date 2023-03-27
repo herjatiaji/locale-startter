14a500d1c9f2e9dea29792aa3a0b1563199b4e5b
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-03-27 13:49:47 +0700

    initial commit

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -9,8 +9,13 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 showHelp();
             }
         });
+
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() +
+                TimeUnit.DAYS.toMillis(5);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
     }
 
     /**
@@ -65,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, HelpActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_language) {
+            Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(languageIntent);
+            return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
